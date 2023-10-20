79b8062e978f9b3dcdf0bc243fdadd8c245b8659
JieunKim <78676660+mellykim123@users.noreply.github.com>
2023-10-20 12:31:55 +0900

    [ADD] router configuration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { useState } from "react";
 import MainPage from "./pages/MainPage";
 import SurveyPage from "./pages/SurveyPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import MapPage from "./pages/MapPage";
+import { RecoilRoot } from "recoil";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 
-function App(props) {
-  return /*<SurveyPage />;*/ <MapPage />;
+function Content() {
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<MainPage />} />
+        <Route exact path="/map" element={<MapPage />} />
+        <Route exact path="/survey" element={<SurveyPage />} />
+      </Routes>
+    </>
+  );
 }
-
+function App() {
+  return (
+    <>
+      <RecoilRoot>
+        {/* <GlobalStyle /> */}
+        <Router>
+          <Content />
+        </Router>
+      </RecoilRoot>
+    </>
+  );
+}
 export default App;
