c0bed79d85d3bfe93bf0daab73480addab94523f
JieunKim <78676660+mellykim123@users.noreply.github.com>
2023-10-28 01:15:56 +0900

    [ADD] Edit 기능

--- src/pages/CheckList/Item/index.jsx
+++ src/pages/CheckList/Item/index.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ todo, completed, onClick, onEditClick, isEditable }) {
+  const [newTodo, setNewTodo] = useState(todo);
+  const handleSubmit = (e) => {
+    console.log(newTodo);
+    e.preventDefault();
+  };
+  const handleEdit = (e) => {
+    console.log(e.target.value);
+    setNewTodo(e.target.value);
+  };
   return (
     <li style={{ listStyleType: "none" }}>
       {isEditable ? (
-        <input
-          type="text"
-          value={todo}
-          onChange={(e) => onEditClick(e.target.value)}
-          onBlur={() => onEditClick(null)}
-        />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e) => handleEdit(e)}
+            onBlur={() => onEditClick(null)}
+          />
+          <button type="submit">submit</button>
+        </form>
       ) : (
         <>
           <input type="checkbox" checked={completed} onChange={onClick} />
