1d5b8ce62b5b6c163857a0a498514006e1041b58
Dain <ekdls5388@korea.ac.kr>
2023-10-27 23:38:24 +0900

    10/27 설문조사

--- src/pages/SurveyPage.jsx
+++ src/pages/SurveyPage.jsx
@@ -30,8 +30,8 @@ const SelectButton = styled.div`
 
 function SurveyPage() {
   const themes = [
-    "목적지",
-    "목적지",
+    "목적지 스타일",
+    "목적지 스타일",
     "즐기는 스타일",
     "즐기는 스타일",
     "즐기는 스타일",
@@ -41,38 +41,52 @@ function SurveyPage() {
   ];
 
   const questions = [
-    "자연 휴양지와 도시 중에 어디에 더 가고 싶어요?",
-    "유명하지 않더라도 소소한 매력이 있는 곳과 유명한 핫 플레이스 중에 어디가 더 좋나요?",
-    "계획에 따른 여행과 무계획 여행 중 어떤 것을 선호하나요?",
-    "타이트한 일정으로 알찬 여행, 여유있는 여행 어떤 게 더 좋아요?",
-    "사진 촬영 중요하다? 안 중요하다?",
-    "현지 맛집 찾아다니는 스타일? 아니면 먹는 건 아무거나 상관없다?",
-    "당일치기 여행이 좋아요? 아니면 여행지 근처에서 숙박할까요?",
-    "편하지만 비싼 숙소가 좋을까요? 아니면 불편하더라도 저렴한 숙소가 좋을까요?",
+    "자연 휴양지에 가깝다면 1, 도시에 가깝다면 5",
+    "한적한 숨은 여행지에 가깝다면 1, 유명 핫 플레이스에 가깝다면 5",
+    "무계획 여행은 1, 무계획적인 여행은 1",
+    "타이트한 일정으로 알차게 여행에 가깝다면 1, 여유있는 여행에 가깝다면 5",
+    "사진 촬영 안 중요하면 1, 사진 촬영 중요하면 5",
+    "맛집 딱히 상관없다면 1, 맛집 식도락 여행하고 싶으면 5",
+    "당일치기 여행에 가깝다면 1, 숙소 필수라면 5",
+    "불편해도 싸면 장땡 1 비싸도 편하길 원하면 5",
     // 다른 질문들을 여기에 추가
   ];
-
+  const answerKeys = [
+    "preferNatureThanCity",
+    "preferNewCity",
+    "preferDetailPlan",
+    "preferTightSchedule",
+    "preferManyPhotos",
+    "preferGoodFood",
+    "preferDayTrip",
+    "preferCheapHotelThanComfort",
+  ];
   const answerOptions = [
-    ["휴양지", "도시"],
-    ["알려지지 않은 방문지", "유명한 방문지"],
-    ["계획에 따른 여행", "무계획 여행"],
-    ["타이트하고 알찬 일정", "여유로운 일정"],
-    ["사진은 필수지", "사진 촬영 안해도 된다"],
-    ["맛집 도장깨기 필수", "먹기만 하면 되지 뭐"],
-    ["여행지는 그저 여행지일 뿐, 당일치기", "여행지에서 자고 갈래"],
-    ["편하다면 비싸도 돼", "불편하지만 저렴한 숙소"],
+    ["1", "2", "3", "4", "5"],
+    ["1", "2", "3", "4", "5"],
+    ["1", "2", "3", "4", "5"],
+    ["1", "2", "3", "4", "5"],
+    ["1", "2", "3", "4", "5"],
+    ["1", "2", "3", "4", "5"],
+    ["1", "2", "3", "4", "5"],
+    ["1", "2", "3", "4", "5"],
     // 다른 질문에 대한 선택지 배열을 추가
   ];
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [currentTheme, setCurrentTheme] = useState(0);
   const [answers, setAnswers] = useState([]);
+  const [currentAnswerKey, setCurrentAnswerKey] = useState(0);
 
-  const handleNextQuestion = (answer) => {
-    setAnswers([...answers, answer]);
-    if (currentQuestion < questions.length - 1) {
+  const handleNextQuestion = (question, answer) => {
+    const answerObject = { [question]: answer };
+    setAnswers([...answers, answerObject]);
+    if (currentQuestion <= questions.length - 1) {
       setCurrentQuestion(currentQuestion + 1);
       setCurrentTheme(currentTheme + 1);
+      setCurrentAnswerKey(currentAnswerKey + 1);
+    } else {
+      console.log("수고요");
     }
   };
 
@@ -82,21 +96,27 @@ function SurveyPage() {
     <SurveyContainer>
       <p className="survey-title">#{themes[currentTheme]}</p>
 
-      <QuestionContainer>
-        <p className="question">{questions[currentQuestion]}</p>
-        <div className="answer-options">
-          {answerOptions[currentQuestion].map((option, index) => (
-            <label key={index}>
-              <SelectButton
-                key={index}
-                onClick={() => handleNextQuestion(option)}
-              >
-                {option}
-              </SelectButton>
-            </label>
-          ))}
-        </div>
-      </QuestionContainer>
+      {currentQuestion < questions.length ? (
+        <QuestionContainer>
+          <p className="question">{questions[currentQuestion]}</p>
+          <div className="answer-options">
+            {answerOptions[currentQuestion].map((option, index) => (
+              <label key={index}>
+                <SelectButton
+                  key={index}
+                  onClick={() =>
+                    handleNextQuestion(answerKeys[currentAnswerKey], option)
+                  }
+                >
+                  {option}
+                </SelectButton>
+              </label>
+            ))}
+          </div>
+        </QuestionContainer>
+      ) : (
+        <p className="completeSurvey">수고하셨습니다!</p>
+      )}
     </SurveyContainer>
   );
 }
