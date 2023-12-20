0f4d5d0e3f141d8d17feadf92e528d024d9c0b87
JieunKim <78676660+mellykim123@users.noreply.github.com>
2023-12-20 21:21:12 +0900

    Update SignUpPage.jsx

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { signupAPI, checkEmailAPI } from "../apis";
+import { signupAPI, checkEmailAPI } from "../apis/signup";
 import { useNavigate } from "react-router-dom";
 import { BsCheck } from "react-icons/bs";
 import {
